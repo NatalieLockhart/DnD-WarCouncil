f9b4639bf227cc343605c18fe5245e7153a65879
NL <natalie.lockhart5@gmail.com>
2019-01-24 17:04:38 -0700

    Improving monsterController test

--- test/monsterControllerTest.js
+++ test/monsterControllerTest.js
@@ -10,14 +10,14 @@ describe('monsterController.getMonster(res, monster)', function (){
 	it('should return a monster object with name "the strongest goblin" (mocked)', function () {
 		
 		//1. ARRANGE
-		var result1 = new Monster("the strongest goblin", "it\'s a goblin",  [], "weaponsHere", "1000");
+		var result1 = new Monster("the strongest goblin", 800, "it\'s a goblin",  [], "weaponsHere", [],"1000");
 		var mock = new MockAdapter(axios);
 		mock.onGet('https://api-beta.open5e.com/monsters/goblin/').reply(200, result1);
 			 
 		//2. ACT
 		mController.getMonster("", "goblin").then(function(response) {
 			//3. ASSERT
-			expect(response.name).to.equal(result1.name);
+			expect(result1).to.deep.equal(response);
 		})
 		.catch(error => { 
 			console.log(error);
